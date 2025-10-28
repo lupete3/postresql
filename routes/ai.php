3673bff0dcedc89996fededdd39ae899e12fbe8e
lupete3 <lupeteplacide@gmail.com>
2025-10-28 09:37:42 +0100

    Premiere version deployee

--- routes/ai.php
+++ routes/ai.php
@@ -1,12 +1,2 @@
 <?php
 
-use Laravel\Mcp\Server\Facades\Mcp;
-use App\Mcp\Servers\WeatherServer;
-
-Mcp::web('demo', \App\Mcp\Servers\PublicServer::class); // Available at /mcp/demo
-Mcp::local('demo', \App\Mcp\Servers\LocalServer::class); // Start with ./artisan mcp:start demo
-
-
-Mcp::web('/mcp/weather', WeatherServer::class);
-
-Mcp::local('weather', WeatherServer::class);
